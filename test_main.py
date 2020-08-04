056782dfd8ff2980897e6f835b5b378b72f2e86d
alvahuiwork@gmail.com <alvahuiwork@gmail.com>
2020-08-04 23:37:15 +0800

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -11,6 +11,7 @@ PASSWORD = 'huff-puff'
 
 @pytest.fixture
 def client():
+    assert False
     os.environ['JWT_SECRET'] = SECRET
     main.APP.config['TESTING'] = True
     client = main.APP.test_client()
@@ -20,12 +21,14 @@ def client():
 
 
 def test_health(client):
+    assert False
     response = client.get('/')
     assert response.status_code == 200
     assert response.json == 'Healthy'
 
 
 def test_auth(client):
+    assert False
     body = {'email': EMAIL,
             'password': PASSWORD}
     response = client.post('/auth', 
